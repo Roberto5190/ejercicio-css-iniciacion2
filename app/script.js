9c1f9a914cf32c7bc973e2557f39cc401c5ec4fb
Roberto Fernández <rfernandez5190@esdmadrid.es>
2025-03-13 00:38:07 +0100

    ejercicio terminado

--- app/script.js
+++ app/script.js
@@ -1,3 +1,7 @@
-document.getElementById("subir").addEventListener("click", function () {
+const button = document.querySelector(".subir")
+
+button.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+console.log(button);
